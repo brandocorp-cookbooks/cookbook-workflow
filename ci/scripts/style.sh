79a445ea97e7142272f567544dde5ac8bc900709
Brandon Raabe <brandon.raabe@ge.com>
2016-10-03 14:27:59 -0700

    Provide output for foodcritic

--- ci/scripts/style.sh
+++ ci/scripts/style.sh
@@ -1,2 +1,9 @@
 #!/bin/bash -e
-cd cookbook-workflow && chef exec foodcritic . 
+SUCCESS='\033[0;32m'
+FAILURE='\033[0;31m'
+CLEAR='\033[0m'
+
+cd cookbook-workflow && echo -n "Running Foodcritic..."
+if chef exec foodcritic . ; then
+  echo -e "${SUCCESS}SUCCESS${CLEAR}"
+fi
